6c67d38f4a9b9cfa24f7a15a9afc81488e46d5fe
eldetulado <eldetulado@gmail.com>
2018-02-01 10:53:23 -0400

    Cambio de colores en el archivo colors.xml, se realiza el diseno, la fucionalidad del boton calcular, falta revisar y corregir algunos errores

--- app/src/main/java/com/example/oso/findagekotlin/MainActivity.kt
+++ app/src/main/java/com/example/oso/findagekotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.oso.findagekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val yearCurrent = Calendar.getInstance().get(Calendar.YEAR)
+
+        button.setOnClickListener {
+            resultAge.text = "${yearCurrent - userYear.text.toString().toInt()}"
+            result.visibility = View.VISIBLE
+        }
     }
 }
